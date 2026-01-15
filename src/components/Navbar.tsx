c1df7110ecf51965f7563e9bc229240c3f849a35
suprimbishwakarma <suprimbishwakarma2002@gmail.com>
2026-01-15 11:15:17 +0545

    feat(ui) : add sticky component at the top

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <header className='bg-[#6f9283] border-b border-gray-700 p-4 sticky top-0 z-10'>
+        <div className='container mx-auto flex justify-between items-center'>
+            <h1 className='text-2xl text-white bg-clip-text'>
+                Rick & Morty Pro
+            </h1>
+        </div>
+    </header>
+  )
+}
+
+export default Navbar
