d91fa76fbecd51801c59a5cbb9deb1492039decd
suprimbishwakarma <suprimbishwakarma2002@gmail.com>
2026-01-15 11:32:04 +0545

    feat(ui) : add simple message to the user

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,10 @@
-import React from "react";
-
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="mb-8">
+      <h2 className="text-3xl font-bold mb-2">Character Explorer</h2>
+      <p className="text-gray-400">Search and filter through this dashboard</p>
+    </div>
+  );
 };
 
 export default Home;
