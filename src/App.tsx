a01bcc4bc69e17595f2b32535f7a0b0ac9871e83
suprimbishwakarma <suprimbishwakarma2002@gmail.com>
2026-01-15 11:17:29 +0545

    feat(routing) : add routing to home page and a charcter detail page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import Navbar from "./components/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import CharacterDetail from "./pages/CharacterDetail";
 
 const App = () => {
   return (
-    <div className='text-red-700'>App</div>
-  )
-}
+    <div className="min-h-screen bg-[#3b3f44] text-white font-sans">
+      <Navbar />
+      <main className="container mx-auto p-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/character/:id" element={<CharacterDetail />} />
+          <Route />
+        </Routes>
+      </main>
+    </div>
+  );
+};
 
-export default App
+export default App;
