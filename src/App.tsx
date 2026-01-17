b751ad861958a44e25fe9485d9c847820c0d3bab
suprimbishwakarma <suprimbishwakarma2002@gmail.com>
2026-01-17 11:25:58 +0545

    feat(route): add new route for a landing Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import Navbar from "./components/Navbar";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import CharacterDetail from "./pages/CharacterDetail";
+import Landing from "./pages/Landing";
 
 const App = () => {
   return (
     <div className="min-h-screen bg-[#3b3f44] text-white font-sans">
-      <Navbar />
-      <main className="container mx-auto p-4">
+      <main className="container mx-auto">
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Landing />} />
+          <Route path="/dashboard" element={<Home />} />
           <Route path="/character/:id" element={<CharacterDetail />} />
           <Route />
         </Routes>
